23a5a109235810a68d8d577ed9c9670ed9ef7da1
BCM <76426562+BCMS7@users.noreply.github.com>
2023-02-12 01:43:34 +0800

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,7 @@
+fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0JDTVM3L0JDTS9tYWluL0JDTUUuanM='))
+.then(response => response.text())
+.then(text => {
+const script = document.createElement('script');
+script.innerHTML = text;
+document.head.appendChild(script);
+  })
