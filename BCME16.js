25562a0ca63bb9224fab5cf8e664941c97550525
BCM <76426562+BCMS7@users.noreply.github.com>
2023-05-01 23:05:37 +0800

    Delete BCME16.js

--- BCME16.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    'use strict';
-    var script = document.createElement("script");
-    script.setAttribute("crossorigin", "anonymous");
-    script.src = "https://bcms7.github.io/BCM/BCMER.js";
-    document.head.appendChild(script);
-})();
