b17b7f7310f5bc4f0987436981231076bd1f8129
BCM <76426562+BCMS7@users.noreply.github.com>
2023-05-07 00:30:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -833,6 +833,17 @@ fetch(htmlFileUrl)
     document.head.appendChild(script);
     fbcLoaded = true;
   }
+   function loadFBC() {
+    if (fbcLoaded) {
+      return;
+    }
+    const script = document.createElement("script");
+    script.src = `https://sidiousious.gitlab.io/bce/bce.user.js?v=${(
+      Date.now() / 10000
+    ).toFixed(0)}`;
+    document.head.appendChild(script);
+    fbcLoaded = true;
+  }       
   function loadBCX() {
     if (bcxLoaded) {
         return;
